e40df990428dd1559ebe9a4c3d8c00eb22c80e1a
LocheZ <1913565532@qq.com>
2023-10-14 17:59:40 +0800

    Upload dataset

--- BPNN.py
+++ BPNN.py
@@ -1,92 +1,69 @@
-import copy
-
+import datetime
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from palmerpenguins import load_penguins
+
+np.random.seed(22)
+
+# 0.加载并处理palmerpenguins数据集
+penguins = load_penguins()
+penguins = penguins[['species', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']]
+penguins.loc[penguins['species'] == 'Adelie', 'species'] = 0
+penguins.loc[penguins['species'] == 'Gentoo', 'species'] = 1
+penguins.loc[penguins['species'] == 'Chinstrap', 'species'] = 2
+penguins.dropna(inplace=True)
 
 
-class BPNN(object):
-    def __init__(self, layer_dims, learning_rate=0.1, seed=22, initializer='he', optimizer='gd'):
-        self.layer_dims = layer_dims
-        self.learning_rate = learning_rate
-        self.seed = seed
-        self.initializer = initializer
-        self.optimizer = optimizer
-
-        self.parameters = None
-        self.costs = None
-
-    def fit(self, x, y, num_epoch=100):
-        m, n = x.shape
-        layer_dims = copy.deepcopy(self.layer_dims)
-        layer_dims.insert(0, n)
-        if y.ndim == 1:
-            y = y.reshape(-1, 1)
-
-        self.parameters = xavier_initializer(layer_dims, self.seed)
-        self.parameters, self.costs = self.gd_optimizer(x, y, self.parameters, num_epoch, self.learning_rate)
-        return self
-
-    def predict(self, x):
-        if self.parameters is None:
-            raise Exception('you must fit before predict')
-
-        a_last, _ = self.forward_L_layer(x, self.parameters)
-        if a_last.shape[1] == 1:
-            predict = np.zeros(a_last.shape)
-            predict[a_last >= 0.5] = 1
-        else:
-            predict = np.argmax(a_last, axis=1)
-        return predict
-
-    def compute_cost(self, y_hat, y):
-        if y.ndim == 1:
-            y = y.reshape(-1, -1)
-        if y.shape[1] == 1:
-            cost = cross_entropy_sigmoid(y_hat, y)
-        return cost
-
-    def backward_one_layer(self, da, cache, activation):
-        (a_pre, w, b, z) = cache
-        m = da.shape[0]
-
-        dz = sigmoid_backward(da, z)
-        dw = np.dot(dz.T, a_pre) / m
-        db = np.sum(dz, axis=0, keepdims=True) / m
-        da_pre = np.dot(dz, w)
-        assert dw.shape == w.shape
-        assert db.shape == b.shape
-        assert da_pre.shape == a_pre.shape
-        return da_pre, dw, db
-
-
-def xavier_initializer(layer_dims, seed=22):
-    np.random.seed(seed)
-    parameters = {}
-    num_L = len(layer_dims)
-    for L in range(num_L - 1):
-        temp_w = np.random.randn(layer_dims[L + 1], layer_dims[L]) * np.sqrt(1 / layer_dims[L])
-        temp_b = np.zeros((1, layer_dims[L + 1]))
-        parameters['W' + str(L + 1)] = temp_w
-        parameters['b' + str(L + 1)] = temp_b
+def initializer(n_x, n_h, n_y):
+    """
+    1.初始化参数
+    :param n_x: 输入层神经元个数
+    :param n_h: 隐藏层神经元个数
+    :param n_y: 输出层神经元个数
+    :return: 初始化的参数
+    """
+    # 权重和偏置矩阵
+    w1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros(shape=(n_h, 1))
+    w2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros(shape=(n_y, 1))
+    # 通过字典保存参数
+    parameters = {'w1': w1, 'b1': b1, 'w2': w2, 'b2': b2}
     return parameters
 
 
-def cross_entropy_sigmoid(y_hat, y):
+def forward_propagation(X, parameters):
     """
-    计算二分类的交叉熵
-    :param y_hat: 模型输出值
-    :param y: 样本真实值
-    :return:
+    2.前向传播
+    :param X: 输入
+    :param parameters: 参数
+    :return: a2, cache
     """
-    loss = -(np.dot(y.T, np.log(y_hat)) + np.dot(1 - y.T, np.log(1 - y_hat))) / y.shape[0]
-    return np.squeeze(loss)
+    w1 = parameters['w1']
+    b1 = parameters['b1']
+    w2 = parameters['w2']
+    b2 = parameters['b2']
+    # 通过前向传播计算a2
+    z1 = np.dot(w1, X) + b1
+    a1 = 1 / (1 + np.exp(-z1))
+    z2 = np.dot(w2, a1) + b2
+    a2 = 1 / (1 + np.exp(-z2))  # 隐层和输出层神经元都使用Sigmoid函数
+    # 通过字典保存参数
+    cache = {'z1': z1, 'a1': a1, 'z2': z2, 'a2': a2}
+    return a2, cache
 
 
-def sigmoid_backward(da, cache_z):
-    a = 1 / (1 + np.exp(-cache_z))
-    dz = da * a * (1 - a)
-    assert dz.shape == cache_z.shape
-    return dz
-
-
+def compute_cost(a2, Y):
+    """
+    3.计算代价函数
+    :param a2: 网络输出
+    :param Y: 真实值
+    :return: cost
+    """
+    m = 3  # 三类企鹅
+    # 采用交叉熵（cross-entropy）作为代价函数
+    log_probs = np.multiply(np.log(a2), Y) + np.multiply((1 - Y), np.log(1 - a2))
+    cost = - np.sum(log_probs) / m
+    return cost
 
